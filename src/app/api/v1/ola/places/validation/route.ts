2792e9234293ce411cafd543e09008741a6515c9
Arshit Chaurasia <arshitcc@gmail.com>
2025-04-03 09:28:48 +0530

    fix(build) : 1

--- src/app/api/v1/ola/places/validation/route.ts
+++ src/app/api/v1/ola/places/validation/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/db";
 import axios from "axios";
 
-export default async function GET(req: NextRequest) {
+export async function GET(req: NextRequest) {
     try {
         await connectDB();
 
