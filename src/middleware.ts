b9e00fdfce9304eff05fe462fa77ac91312b1e77
Arshit Chaurasia <arshitcc@gmail.com>
2025-04-03 09:37:30 +0530

    fix : 3

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,14 @@ export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (token) {
-    if (pathname.startsWith("/login") || pathname.startsWith("/signup") || pathname.startsWith("/")) {
+    if (pathname.startsWith("/login") || pathname.startsWith("/signup")) {
       return NextResponse.redirect(new URL("/profile", req.url));
     }
   }
 
   if (
     !token &&
-    (pathname.startsWith("/profile") || pathname.startsWith("/") || pathname.startsWith('/home') || pathname.startsWith("/api/:path*"))
+    (pathname.startsWith("/profile") || pathname.startsWith('/home') || pathname.startsWith("/api/:path*"))
   ) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
@@ -21,5 +21,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/login", "/signup", "/home", "/profile/:path*", "/api/:path*", "/"],
+  matcher: ["/login", "/signup", "/home", "/profile/:path*", "/api/:path*"],
 };
